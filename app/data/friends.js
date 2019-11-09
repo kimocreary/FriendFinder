fa1fc734f6216362ec793a20fcaa2c3b350cbfcd
Kimberlyn Creary <kimocreary@gmail.com>
2019-11-09 10:03:04 -0500

    friends added

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,13 @@ var friends = [
         name: "SpongeBob",
         photo: "https://www.google.com/url?sa=i&source=imgres&cd=&ved=2ahUKEwjuuLqRh9vlAhUxnq0KHVkNCe0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F502503270894221343%2F&psig=AOvVaw2U7irjRab6gwDY3MDRjEQL&ust=1573317656019874",
         score: [5, 4, 3, 2, 1, 4, 5, 3, 2, 1]
+    
+    
+        name: "Dwight",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiCrpT_mdvlAhVFQq0KHeLTA-UQjRx6BAgBEAQ&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FDwight_Schrute&psig=AOvVaw0VvIecbBO79QQVpe4cp9at&ust=1573322714012981",
+        score: [2, 4, 3, 2, 4, 3, 1, 3, 4, 5]
+
+        name: ""
     }
 ]
 // add 9 friends
