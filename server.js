b495bdf5a77dabcd1bd5e78f9d649823ef46b9fb
Kimberlyn Creary <kimocreary@gmail.com>
2019-11-09 14:09:11 -0500

    route updated

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require("./app/routing/apiRoutes")(app)
 require("./app/routing/htmlRoutes")(app)
 app.listen(PORT, function(){
-    console.log("Listening on this Port")
+    console.log(`Listening on this port: ${PORT}`)
 })
